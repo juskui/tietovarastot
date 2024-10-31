e761e75ad9cc385fb6c0b2a347b4449a720201b4
Jussi Kuikka <jk026110@edu.taitotalo.fi>
2024-10-31 13:29:56 +0200

    13:29

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 import sqlite_con as db  # sqlite_con.py samasta hakemistosta
+import pandas as pd
+import numpy as np
+import csv
 
 connection = db.conn
 
-x: str = "kods"
+# sql = "select * from movies"
 
-x + 2
+rows = connection.execute(  # rows type = cursor = current set of records
+    "select title\
+     from movies\
+     where title like 'A%'\
+    "
+)
+
+# with open("titles-csv.csv", "w", newline="") as csvfile:
+#     csvwriter = csv.writer(csvfile)
+#     csvwriter.writerow([i[0] for i in rows.description])  # write headers
+#     csvwriter.writerows(rows.fetchall())
+
+# viedään resultset DataFrameksi
+df = pd.DataFrame(rows.fetchall())
+print(df)
+
+# tallennetaan csv-tiedostoon
+df = pd.read_sql("Select * from movies", connection)
+df.to_csv("titles-3.csv", index=True)
+
+# tallennetaan json:ksi
+df.to_json("titles.json", index=True)
+
+# tallenna listaan
+lista = []
+for row in rows:
+    lista.append(row)
+print(lista)
+
+
+# with open('dump.sql', 'w') as f:
+#     for line in con.iterdump():
+#         f.write('%s\n' % line)
+# con.close()
+
+# for row in rows:
+#     print(row)
+
+# a = rows.fetchone()
+# print((a))
+
+# haetaan seuraava tulos
+# iteraattori = iter(rows)
+# rivi = next(iteraattori)  # next siirtyy indexissä yhdellä eteenpäin
+# rivi2 = next(iteraattori)
+
+# print(rivi, rivi2)
+
+# connection.close()
--- sqlite_con.py
+++ sqlite_con.py
@@ -1,9 +1,16 @@
+# import sqlite3
+# from sqlite3 import Error
+
+# try:
+#     conn = sqlite3.connect("./databases/movies.db")
+# except Error as e:
+#     print(e)
+
 import sqlite3
 from sqlite3 import Error
 
 try:
-    conn = sqlite3.connect("./databases/movies.db")
+    with sqlite3.connect("./databases/movies.db") as conn:
+        pass  # Your database operations go here
 except Error as e:
     print(e)
-
-print(None)
